d0eed1b10e189338ed44af0525b58cbcb6e3e0ef
StanDenisov <git@warlord.dev>
2020-11-10 06:08:51 +0300

    init

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    private Rigidbody _myBody;
+    private float _moveForce = 10f;
+
+    private void Awake()
+    {
+        _myBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        _myBody.velocity = new Vector3(-h * _moveForce, 0f, 0f );
+    }
+}
---
+++ Assets/Scripts/PumpkinScript.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PumpkinScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -3f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Golem"))
+        {
+            Destroy(other.gameObject);
+            Time.timeScale = 0f;
+        }
+    }
+}
